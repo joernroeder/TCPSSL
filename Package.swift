88a7c0a24d1c7ba838b3f25b4ce01c07bf3f66f7
Jörn Röder <kontakt@joernroeder.de>
2016-08-26 15:50:25 +0200

    pointed OpenSSL to joernroeder/OpenSSL

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "TCPSSL",
     dependencies: [
         .Package(url: "https://github.com/VeniceX/TCP.git", majorVersion: 0, minor: 7),
-        .Package(url: "https://github.com/Zewo/OpenSSL.git", majorVersion: 0, minor: 7),
+        .Package(url: "https://github.com/joernroeder/OpenSSL.git", majorVersion: 0, minor: 7),
     ]
 )
